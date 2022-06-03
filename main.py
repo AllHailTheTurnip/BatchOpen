e22e0089fc2f88cd94f9083d882a5906777a7b73
MatthewMcMarvelous <terminatorlincoln@yahoo.com>
2022-06-02 21:19:42 -0400

    Updated it. Create a .txt file in the same directory as the .exe, containing on each line a link to a different program. Then, add in the name of the program to the shortcut's arguments line in between quotation marks.

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-# Written by @MightyTurnip.
-# To use...
-# Step 1: Package with pyinstaller as an .exe
-# Step 2: Create a shortcut to the .exe
-# Step 3: In target, add in-between double-quotes a link for the programs you want connected.
-# Note: Each connected program needs a to be within its own double-quotes.
-
 import os
+import subprocess
+from os.path import exists
+
+pathName: str
 
 if __name__ == '__main__':
     import sys
 
-    # Check if list is empty or not.
     if len(sys.argv) <= 1:
-        print("No links were provided.")
-    else:
-        DETACHED_PROCESS = 0x00000008
-        for x in range(1, len(sys.argv)):
-            # Open all links.
-            try:
-                os.startfile(sys.argv[x])
-            except:
-                print("Not a link.")
+        print("No path file found.")
+        input()
+        quit()
+
+    pathName = sys.argv[1]
+
+    if not exists(pathName):
+        file = open(pathName, 'r')
+        file.close()
+
+    DETACHED_PROCESS = 0x00000008
+    file = open(pathName, 'r')
+
+    print("Opening files")
+
+    length = file.readlines()
+    if length == 0:
+        print("No links found.")
+        input()
+        quit()
+
+    file.seek(0, 0)
+    for link in file.readlines():
+        try:
+            finalLink = link.replace('\n', '')
+            print(finalLink)
+            os.startfile(finalLink)
+        except:
+            print("Failed to open link.")
+
+    input()
